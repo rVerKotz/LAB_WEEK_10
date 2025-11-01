80ce03758add6351ee23b69dfc27b8e7a25853fe
rVerKotz <savero2605@gmail.com>
2025-11-01 22:00:53 +0700

    Part 3 - Assignment

--- app/src/main/java/eu/tutorials/lab_week_10/MainActivity.kt
+++ app/src/main/java/eu/tutorials/lab_week_10/MainActivity.kt
@@ -4,13 +4,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import eu.tutorials.lab_week_10.viewmodels.TotalViewModel
 import androidx.lifecycle.ViewModelProvider
 import eu.tutorials.lab_week_10.database.Total
 import eu.tutorials.lab_week_10.database.TotalDatabase
 import androidx.room.Room
+import eu.tutorials.lab_week_10.database.TotalObject
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class MainActivity : AppCompatActivity() {
+
+    private fun getCurrentDateTimeString(): String {
+        val sdf = SimpleDateFormat("yyyy-MM-dd HH:mm:ss", Locale.getDefault())
+        return sdf.format(Date())
+    }
     private val ID: Long = 1
     private fun prepareDatabase(): TotalDatabase {
         return Room.databaseBuilder(
@@ -19,12 +29,25 @@ class MainActivity : AppCompatActivity() {
         ).allowMainThreadQueries().build()
     }
     private fun initializeValueFromDatabase() {
-        val total = db.totalDao().getTotal(ID)
-        if (total.isEmpty()) {
-            db.totalDao().insert(Total(id = 1, total = 0))
+        val totalList = db.totalDao().getTotal(ID)
+
+        if (totalList.isEmpty()) {
+            val now  = getCurrentDateTimeString()
+            val newTotalObject = TotalObject(value = 0, date = getCurrentDateTimeString())
+            db.totalDao().insert(Total(id = ID, total = newTotalObject))
+
+            viewModel.setTotal(0)
+
+            Toast.makeText(this, "Create new instance: $now", Toast.LENGTH_LONG).show()
         } else {
-            viewModel.setTotal(total.first().total)
+            val existingTotalObject = totalList.first().total
+            viewModel.setTotal(existingTotalObject.value)
+
+            val lastUpdateDate = existingTotalObject.date
+            Toast.makeText(this, "Last updated: $lastUpdateDate", Toast.LENGTH_LONG).show()
         }
+
+
     }
 
 
@@ -59,6 +82,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onPause() {
         super.onPause()
-        db.totalDao().update(Total(ID, viewModel.total.value!!))
+        val currentValue = viewModel.total.value ?: 0
+        val updatedTotalObject = TotalObject(value = currentValue, date = getCurrentDateTimeString())
+
+        db.totalDao().update(Total(id = ID, total = updatedTotalObject))
     }
 }
--- app/src/main/java/eu/tutorials/lab_week_10/database/Total.kt
+++ app/src/main/java/eu/tutorials/lab_week_10/database/Total.kt
@@ -1,6 +1,7 @@
 package eu.tutorials.lab_week_10.database
 
 import androidx.room.ColumnInfo
+import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
@@ -10,10 +11,12 @@ data class Total(
     @ColumnInfo(name = "id")
     val id: Long = 0,
 
-    @ColumnInfo(name = "total")
-    val total: Int = 0,
 
-) {
+    @Embedded val total: TotalObject
 
+)
 
-}
+data class TotalObject(
+    @ColumnInfo(name = "value") val value: Int,
+    @ColumnInfo(name = "date") val date: String
+)
