2fbf7fdfada4c6fca11e41cbbbbbd2ce21ebd8dd
rVerKotz <savero2605@gmail.com>
2025-11-01 21:11:55 +0700

    Part 1.2 - App with View Model and problems with Fragment

---
+++ app/src/main/java/eu/tutorials/lab_week_10/FirstFragment.kt
@@ -0,0 +1,52 @@
+package eu.tutorials.lab_week_10
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.lifecycle.ViewModelProvider
+import eu.tutorials.lab_week_10.viewmodels.TotalViewModel
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        prepareViewModel()
+    }
+    private fun updateText(total: Int) {
+        view?.findViewById<TextView>(R.id.text_total)?.text =
+            getString(R.string.text_total, total)
+    }
+    private fun prepareViewModel(){
+        val viewModel =
+            ViewModelProvider(this).get(TotalViewModel::class.java)
+        updateText(viewModel.total)
+    }
+    companion object {
+        fun newInstance(param1: String, param2: String) =
+            FirstFragment()
+    }
+
+}
--- app/src/main/java/eu/tutorials/lab_week_10/MainActivity.kt
+++ app/src/main/java/eu/tutorials/lab_week_10/MainActivity.kt
@@ -6,33 +6,29 @@ import android.widget.Button
 import android.widget.TextView
 import eu.tutorials.lab_week_10.R
 import eu.tutorials.lab_week_10.viewmodels.TotalViewModel
+import androidx.lifecycle.ViewModelProvider
 class MainActivity : AppCompatActivity() {
-//    private val viewModel by lazy {
-//        ViewModelProvider(this)[TotalViewModel::class.java]
-//    }
+    private val viewModel by lazy {
+        ViewModelProvider(this)[TotalViewModel::class.java]
+    }
     private var total: Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        total = 0
-
-        updateText()
-
-        findViewById<Button>(R.id.button_increment).setOnClickListener {
-            incrementTotal()
-        }
+        prepareViewModel()
     }
 
-    // FIX: Function definition doesn't need parameters
-    private fun incrementTotal() {
-        total++
-        updateText()
-    }
 
-    // FIX: Function definition doesn't need parameters
-    private fun updateText() {
+    private fun updateText(total: Int) {
         findViewById<TextView>(R.id.text_total).text =
-            getString(R.string.text_total, total) // Just pass 'total' as the second argument
+            getString(R.string.text_total, total)
+    }
+    private fun prepareViewModel(){
+        updateText(viewModel.total)
+        findViewById<Button>(R.id.button_increment).setOnClickListener {
+            viewModel.incrementTotal()
+            updateText(viewModel.total)
+        }
     }
 }
